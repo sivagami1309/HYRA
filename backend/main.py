d8ad7c622e2797a6909206620e0b385f6440e485
Sivagami S <YOUR_GITHUB_EMAIL>
2026-09-20 18:27:35 +0530

    Enable CORS for Vercel frontend

--- backend/main.py
+++ backend/main.py
@@ -32,7 +32,9 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://hyra-jade.vercel.app",
+        "http://127.0.0.1:5500",
+        "http://localhost:5500",],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
